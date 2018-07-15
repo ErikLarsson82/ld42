4a0a65203f0cb49df50f78c7f4a9ff05dc9edb03
Erik Larsson <erik.mattias.larsson@gmail.com>
2018-07-15 23:02:16 +0200

    Chasers, freezers and fire

--- main.js
+++ main.js
@@ -10,23 +10,36 @@ app.stage.addChild(background)
 
 PIXI.loader
   .add("player.png")
+  .add("speed.png")
   .add("fire.png")
+  .add("freeze.png")
+  .add("freezed.png")
   .add("enemy.png")
+  .add("chaser.png")
+  .add("pylon.png")
   .add("check.png")
   .add("cross.png")
   .load(setup)
 
-let player
-let fires
-let enemies
-let checks
-let playerArrows = [false, false, false, false] //left, up, right, down
-let combos = [
-  "fire",
-  "freeze",
-  "speed"
-]
-let currentCombo
+let player,
+  gameover,
+  speed,
+  pylons,
+  fires,
+  freezes,
+  enemies,
+  chasers,
+  checks,
+  currentCombo,
+  speedTimer,
+  playerArrows = [false, false, false, false], //left, up, right, down
+  combos = [
+    "fire",
+    "freeze",
+    "speed"
+  ],
+  alphabet = "abcdefghijklmnopqrstuvxyz"
+
 
 function setup() {
   player = new PIXI.Sprite(
@@ -36,27 +49,47 @@ function setup() {
   app.stage.addChild(player)
   app.ticker.add(delta => gameLoop(delta))
 
+  speed = new PIXI.Sprite(
+    PIXI.loader.resources["speed.png"].texture
+  )
+  speed.visible = false
+  speed.x = -8
+  speed.y = 0
+  player.addChild(speed)
+
   currentCombo = ""
 
+  gameover = false
+
+  pylons = []
+  freezes = []
   fires = []
   enemies = []
+  chasers = []
   checks = []
+
+  speedTimer = null
 
   loadMap()
 }
 
 function gameLoop(delta){
+  if (gameover)
+    return
+  
+  let multiplier = speedTimer !== null ? 3 : 1
+
   if (playerArrows[0]) {
-    player.x -= 1
+    player.x -= 1 * multiplier
   }
   if (playerArrows[1]) {
-    player.y -= 1
+    player.y -= 1 * multiplier
   }
   if (playerArrows[2]) {
-    player.x += 1
+    player.x += 1 * multiplier
   }
   if (playerArrows[3]) {
-    player.y += 1
+    player.y += 1 * multiplier
   }
   fires.forEach(fire => {
     enemies.forEach(enemy => {
@@ -66,83 +99,99 @@ function gameLoop(delta){
       }
     })
   })
+
+  chasers.forEach(chaser => {
+    freezes.forEach(freeze => {
+      if (hitTestRectangle(chaser, freeze)) {
+        chaser.freeze()
+        freeze.kill()
+      }
+    })
+
+    if (hitTestRectangle(chaser, player)) {
+      gameover = true
+      console.log('Game over')
+    }
+  })
+
+  pylons.forEach(pylon => {
+    if (hitTestRectangle(player, pylon)) {
+      pylon.kill()
+    }
+  })
+
+  if (pylons.length === 0) {
+    gameover = true
+    console.log('Game over')
+  }
+  
+  if (speedTimer !== null) {
+    if (speedTimer < 0) {
+      speedTimer = null
+      speed.visible = false      
+    } else {
+      speedTimer -= delta
+    }
+  }
 }
 
 function loadMap() {
   createEnemy(50, 50)
   createEnemy(80, 50)
   createEnemy(80, 100)
+  createPylon(100, 200)
+  createPylon(150, 200)
+  createPylon(200, 200)
+  createChaser(300, 300)
 }
 
 function hitTestRectangle(r1, r2) {
-
-  //Define the variables we'll need to calculate
   let hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
-
-  //hit will determine whether there's a collision
   hit = false;
-
-  //Find the center points of each sprite
   r1.centerX = r1.x + r1.width / 2;
   r1.centerY = r1.y + r1.height / 2;
   r2.centerX = r2.x + r2.width / 2;
   r2.centerY = r2.y + r2.height / 2;
-
-  //Find the half-widths and half-heights of each sprite
   r1.halfWidth = r1.width / 2;
   r1.halfHeight = r1.height / 2;
   r2.halfWidth = r2.width / 2;
   r2.halfHeight = r2.height / 2;
-
-  //Calculate the distance vector between the sprites
   vx = r1.centerX - r2.centerX;
   vy = r1.centerY - r2.centerY;
-
-  //Figure out the combined half-widths and half-heights
   combinedHalfWidths = r1.halfWidth + r2.halfWidth;
   combinedHalfHeights = r1.halfHeight + r2.halfHeight;
-
-  //Check for a collision on the x axis
   if (Math.abs(vx) < combinedHalfWidths) {
-
-    //A collision might be occuring. Check for a collision on the y axis
     if (Math.abs(vy) < combinedHalfHeights) {
-
-      //There's definitely a collision happening
       hit = true;
     } else {
-
-      //There's no collision on the y axis
       hit = false;
     }
   } else {
-
-    //There's no collision on the x axis
     hit = false;
   }
-
-  //`hit` will be either `true` or `false`
   return hit;
 };
 
 window.addEventListener("keydown", e => {
-  e.preventDefault()
+  
   if (e.keyCode === 37) {
     playerArrows[0] = true
-  }
-  if (e.keyCode === 38) {
+    e.preventDefault()
+  } else if (e.keyCode === 38) {
     playerArrows[1] = true
-  }
-  if (e.keyCode === 39) {
+    e.preventDefault()
+  } else if (e.keyCode === 39) {
     playerArrows[2] = true
-  }
-  if (e.keyCode === 40) {
+    e.preventDefault()
+  } else if (e.keyCode === 40) {
     playerArrows[3] = true
+    e.preventDefault()
+  } else if (alphabet.indexOf(e.key) !== -1) {
+    e.preventDefault()
   }
 })
 
 window.addEventListener("keyup", e => {
-  e.preventDefault()
   if (e.keyCode === 37) {
     playerArrows[0] = false
     currentCombo = ""
@@ -155,7 +204,8 @@ window.addEventListener("keyup", e => {
   } else if (e.keyCode === 40) {
     playerArrows[3] = false
     currentCombo = ""
-  } else {
+  } else if (alphabet.indexOf(e.key) !== -1) {
+    e.preventDefault()
     currentCombo += e.key
     handleCombos()
   }
@@ -201,6 +251,13 @@ function handleCombos() {
     if (targetCombo === "fire") {
       createFires()
     }
+    if (targetCombo === "freeze") {
+      createFreezes()
+    }
+    if (targetCombo === "speed") {
+      speedTimer = 100
+      speed.visible = true
+    }
     animateSucess()
 
     currentCombo = ""
@@ -228,8 +285,8 @@ function createFire() {
     PIXI.loader.resources["fire.png"].texture
   )
   fires.push(fire)
-  fire.x = player.x
-  fire.y = player.y
+  fire.x = player.x + Math.round(player.width/2)
+  fire.y = player.y + Math.round(player.height/2)
   let fireTime = 80
   let speed = 1
   let fireDirection = { 
@@ -264,6 +321,69 @@ function createFires() {
   }
 }
 
+function createFreeze() {
+  let freeze = new PIXI.Sprite(
+    PIXI.loader.resources["freeze.png"].texture
+  )
+  freezes.push(freeze)
+  freeze.x = player.x + Math.round(player.width/2) + Math.round((Math.random() - 0.5) * 70)
+  freeze.y = player.y + Math.round(player.height/2) + Math.round((Math.random() - 0.5) * 70)
+  let freezeTime = 600
+  let speed = 0.02
+  let freezeDirection = { 
+    x: (Math.random() - 0.5) * 2,
+    y: (Math.random() - 0.5) * 2
+  }
+
+  app.stage.addChild(freeze)
+
+  let tickz = delta => {
+    freezeTime -= delta
+    freeze.x += freezeDirection.x * speed
+    freeze.y += freezeDirection.y * speed
+    if (freezeTime < 0) {
+      freeze.kill()
+    }
+  }
+
+  freeze.kill = function() {
+    app.stage.removeChild(freeze)
+    app.ticker.remove(tickz)
+    freezes = freezes.filter(x => x !== freeze)
+  }
+
+  app.ticker.add(tickz)
+}
+
+function createFreezes() {
+  for (let i = 0; i < 4; i++) {
+    createFreeze()
+  }
+}
+
+function createPylon(x, y) {
+  let pylon = new PIXI.Sprite(
+    PIXI.loader.resources["pylon.png"].texture
+  )
+  pylons.push(pylon)
+  pylon.x = x
+  pylon.y = y
+
+  pylon.kill = function() {
+    app.stage.removeChild(pylon)
+    app.ticker.remove(tickz)
+    pylons = pylons.filter(x => x !== pylon)
+  }
+  
+  app.stage.addChild(pylon)
+
+  let tickz = delta => {
+    
+  }
+
+  app.ticker.add(tickz)
+}
+
 function createEnemy(x, y) {
   let enemy = new PIXI.Sprite(
     PIXI.loader.resources["enemy.png"].texture
@@ -282,6 +402,55 @@ function createEnemy(x, y) {
 
   let tickz = delta => {
     
+  }
+
+  app.ticker.add(tickz)
+}
+
+function createChaser(x, y) {
+  let chaser = new PIXI.Sprite(
+    PIXI.loader.resources["chaser.png"].texture
+  )
+  chasers.push(chaser)
+  chaser.x = x
+  chaser.y = y
+
+  let timeout
+
+  let freezed = new PIXI.Sprite(
+    PIXI.loader.resources["freezed.png"].texture
+  )
+  freezed.x = -8
+  freezed.y = -8
+  freezed.visible = false
+  chaser.addChild(freezed)
+
+  chaser.kill = function() {
+    app.stage.removeChild(chaser)
+    app.ticker.remove(tickz)
+    chasers = chasers.filter(x => x !== chaser)
+  }
+
+  chaser.freeze = function() {
+    timeout = 400
+    freezed.visible = true
+  }
+  
+  app.stage.addChild(chaser)
+
+  const chaseSpeed = 0.1
+
+  let tickz = delta => {
+    if (freezed.visible === false) {
+      chaser.x += chaseSpeed * (chaser.x > player.x ? -1 : 1)
+      chaser.y += chaseSpeed * (chaser.y > player.y ? -1 : 1)
+    } else {
+      timeout -= delta
+    }
+    if (timeout < 0) {
+      timeout = null
+      freezed.visible = false
+    }
   }
 
   app.ticker.add(tickz)
